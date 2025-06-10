0934ddc18cea4ce216c36be7cf3ab8fcbc422bc1
Rafael Peixoto <110791857+rafaelpeixoto7559@users.noreply.github.com>
2025-06-10 10:31:25 -0300

    Update piper.py

--- piper.py
+++ piper.py
@@ -89,6 +89,7 @@ def main():
             print(response)
             os.system(f"touch output{i}.wav")
             os.system(f"echo \"{response}\" | ./piper.exe -m ./pt_BR-faber-medium.onnx -f ./output{i}.wav")
+            echo f'{response}' | piper \ --model pt_BR-faber-medium.onnx \ --output_file outputi.wav
             playsound(f'./output{i}.wav')
             i += 1
             
